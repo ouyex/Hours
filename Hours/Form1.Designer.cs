d75fd4e94807c225776d57f7d59fd1148523762c
ouyex <hikarisocial@proton.me>
2022-07-12 14:30:51 -0500

    Add project files.

---
+++ Hours/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace Hours
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.currentLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.startButton.Location = new System.Drawing.Point(12, 61);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(266, 37);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.Location = new System.Drawing.Point(284, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 46);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Entries";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Light", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 41);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Hours";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Location = new System.Drawing.Point(152, 9);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(45, 20);
+            this.totalLabel.TabIndex = 5;
+            this.totalLabel.Text = "Total:";
+            // 
+            // currentLabel
+            // 
+            this.currentLabel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.currentLabel.AutoSize = true;
+            this.currentLabel.Location = new System.Drawing.Point(152, 35);
+            this.currentLabel.Name = "currentLabel";
+            this.currentLabel.Size = new System.Drawing.Size(60, 20);
+            this.currentLabel.TabIndex = 6;
+            this.currentLabel.Text = "Current:";
+            // 
+            // exitButton
+            // 
+            this.exitButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.exitButton.Location = new System.Drawing.Point(284, 61);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(101, 37);
+            this.exitButton.TabIndex = 7;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 110);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.currentLabel);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.startButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hours";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label currentLabel;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Hours/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hours
+{
+    public partial class Form1 : Form
+    {
+        private List<DateTime> startTimes = new List<DateTime>();
+        private List<DateTime> stopTimes = new List<DateTime>();
+
+        private string dataDirPath;
+        private string startTimesDataPath;
+        private string stopTimesDataPath;
+
+        private int workingStopTimePath;
+        private bool running;
+
+        Timer tickTimer = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            tickTimer.Interval = 200;
+            tickTimer.Tick += new EventHandler(Tick);
+            tickTimer.Start();
+
+
+            dataDirPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Hours";
+            if (!Directory.Exists(dataDirPath))
+                Directory.CreateDirectory(dataDirPath);
+            startTimesDataPath = dataDirPath + "\\startTimes.txt";
+            stopTimesDataPath = dataDirPath + "\\stopTimes.txt";
+        }
+
+        private void CreateAndStartNewEntry()
+        {
+            ReadTimes();
+            workingStopTimePath = stopTimes.Count;
+            startTimes.Add(DateTime.Now);
+            stopTimes.Add(DateTime.Now);
+            running = true;
+        }
+
+        private void Tick(object sender, EventArgs e)
+        {
+            if (running)
+            {
+                stopTimes[workingStopTimePath] = DateTime.Now;
+                WriteTimes();
+            }
+
+            ReadTimes();
+            totalLabel.Text =   "Total: " + Math.Round(GetTotalTime(), 2).ToString() + "h";
+
+            if (running)
+            {
+                currentLabel.Text = "Current: " + Math.Round(GetCurrentTime(), 2).ToString() + "h";
+            }
+        }
+
+        private void ReadTimes()
+        {
+            if (!File.Exists(startTimesDataPath) || !File.Exists(stopTimesDataPath))
+                return;
+
+            startTimes.Clear();
+            stopTimes.Clear();
+
+            string[] startFileLines = File.ReadAllLines(startTimesDataPath);
+            string[] stopFileLines = File.ReadAllLines(stopTimesDataPath);
+
+            foreach (string startTimeLine in startFileLines)
+            {
+                try
+                {
+                    startTimes.Add(DateTime.Parse(startTimeLine));
+                }
+                catch { }
+            }
+
+            foreach (string stopTimeLine in stopFileLines)
+            {
+                try
+                {
+                    stopTimes.Add(DateTime.Parse(stopTimeLine));
+                }
+                catch { }
+            }
+        }
+
+        private void WriteTimes()
+        {
+            File.WriteAllText(startTimesDataPath, "");
+            File.WriteAllText(stopTimesDataPath, "");
+
+            foreach (DateTime startTime in startTimes)
+            {
+                File.AppendAllText(startTimesDataPath, startTime.ToString() + "\n");
+            }
+
+            foreach (DateTime stopTime in stopTimes)
+            {
+                File.AppendAllText(stopTimesDataPath, stopTime.ToString() + "\n");
+            }
+        }
+
+
+        private float GetTotalTime()
+        {
+            float totalHours = 0;
+
+            for (int i = 0; i < startTimes.Count; i++)
+            {
+                TimeSpan dif = stopTimes[i].Subtract(startTimes[i]);
+                totalHours += (float)dif.TotalHours;
+            }
+
+            return totalHours;
+        }
+
+        private float GetCurrentTime()
+        {
+            if (stopTimes.Count <= 0 || startTimes.Count <= 0)
+                return 0f;
+
+            TimeSpan dif = stopTimes[stopTimes.Count - 1].Subtract(startTimes[startTimes.Count - 1]);
+            return (float)dif.TotalHours;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (running)
+            {
+                startButton.Text = "Start";
+                running = false;
+            }
+            else
+            {
+                CreateAndStartNewEntry();
+                startButton.Text = "Stop";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
